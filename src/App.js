3d1f32f863dea3c2113d6ae8659aab2b001d69fd
prhmalzd <prhmalzd@gmail.com>
2022-04-06 21:47:16 +0430

    editing some styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import HomePage from './pages/homePage/HomePage'
-// import SignUp from './pages/signUp/Signup'
-// import Worlds from './pages/worlds/Worlds'
-// import Cities from './pages/Cities/Cities'
+import HomePage from './pages/homePage/HomePage'
+import SignUp from './pages/signUp/Signup'
+import Worlds from './pages/worlds/Worlds'
+import Cities from './pages/Cities/Cities'
 import Avatars from './pages/avatars/Avatars'
 
 function App() {
-  return ( <
-    Avatars / >
+  return ( 
+    <Avatars />
   );
 }
 
